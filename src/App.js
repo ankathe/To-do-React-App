2ce314d0f8fa99b731199b6b78e88f46b7b5d919
Katherine <angiecruzo.o@gmail.com>
2023-12-11 19:52:50 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import logo from './images/Projects-logo.png';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container-logo">
+        <img 
+        src={logo} 
+        className="img-logo" />  
+      </div> 
+    </div>
+  );
+}
+
+export default App;
