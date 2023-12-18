1fc79d5d5f036e88c5888c0694f372722a70ed3c
katherine <angiecruzo.o@gmail.com>
2023-12-18 18:06:15 -0500

    add more commands for github pages publish

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import "./App.css";
 import logo from "./images/Projects-logo.png";
 import ListTask from "./components/ListTask";
-import { useState } from "react";
 
-function App() {
-  const [task, setTask] = useState([]);
-  
+function App() {  
   return (
     <div className="App">
       <div className="container-logo">
-        <img src={logo} className="img-logo" />
+        <img alt="logo" src={logo} className="img-logo"/>
       </div>
       <div className="container-todo-principal">
       <div className="title-container">
